cf5f73a8885b1403e05a7cbf1a17bb24e6b541c3
maypatricia <35418099+maypatricia@users.noreply.github.com>
2018-01-14 17:27:56 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -29,3 +29,18 @@ if(!is_null($events)){
             break;  
     }
 }
+$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			echo $result . "\r\n";
+		}
+	}
+}
+echo "OK";
